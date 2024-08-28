78332b18931e4dd099e5b7a4a3c2884844028621
Lê Tuấn Kha <tuankha.dev@gmail.com>
2024-08-28 16:13:19 +0700

    feat: building cli for users (#4)

--- Program.cs
+++ Program.cs
@@ -1,9 +1,40 @@
 ﻿namespace GitHub_Activity_CLI;
 
-class Program
+public static class Program
 {
-    static void Main(string[] args)
+    private static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        const string nameValidation = "\nPlease enter the username to fetch data.";
+        const string helpMessage = "Use \"help\" for more information.\n";
+        Console.WriteLine("---------------------------------GitHub Activity CLI---------------------------------");
+        Console.WriteLine(helpMessage);
+        string? userInput;
+        do
+        {
+            userInput = UserInput();
+            if (string.IsNullOrEmpty(userInput))
+            {
+                Console.WriteLine(nameValidation + " " + helpMessage);
+            }
+            if (userInput == "help")
+            {
+                PrintHelp();
+            }
+        } while(userInput != "exit");
+    }
+    /// <summary>Prompts the user for input and returns the trimmed input string.</summary>
+    /// <returns>The user's input</returns>
+    private static string? UserInput()
+    {
+        Console.Write("github-activity ");
+        return Console.ReadLine()?.Trim();
+    }
+    /// <summary>Displays help message.</summary>
+    private static void PrintHelp()
+    {
+        Console.WriteLine("\nAvailable commands and usage instructions:");
+        Console.WriteLine("<username> - Fetches data for the given username.");
+        Console.WriteLine("help - Displays help message.");
+        Console.WriteLine("exit - Exits the program.\n");
     }
 }
