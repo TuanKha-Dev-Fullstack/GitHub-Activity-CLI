db5ffdc390e57e20f3f3cdecfe2bea013e0afde2
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-09-04 21:43:12 +0700

    feat: create models

---
+++ models/Activity.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GitHub_Activity_CLI.models;
+
+public class Activity
+{
+	[JsonPropertyName("id")]
+	public int Id { get; set; }
+	[JsonPropertyName("type")]
+    public required string Type { get; set; }
+    public required Actor Actor { get; set; }
+    public required Repository Repository { get; set; }
+    public required Payload Payload { get; set; }
+}
