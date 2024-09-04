58ae2b17b7e9fb582926e2ea6963e319b4c89b34
Lê Tuấn Kha <tuankha.dev@gmail.com>
2024-09-04 21:44:18 +0700

    feat: create models (#8)

---
+++ models/Payload.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GitHub_Activity_CLI.models;
+
+public class Payload
+{
+    [JsonPropertyName("action")]
+    public required string Action { get; set; }
+}
---
+++ models/Repository.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GitHub_Activity_CLI.models;
+
+public class Repository
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+    [JsonPropertyName("name")]
+    public required string Name { get; set; }
+    [JsonPropertyName("url")]
+    public required string Url { get; set; }
+}
